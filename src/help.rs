cc6f048d7491e6f4efecc4ac0230850c54beed6f
Brian Pearce <brian.o.pearce@gmail.com>
2014-07-23 00:35:48 -0700

    Add help file to deal with help related material.

---
+++ src/help.rs
@@ -0,0 +1,19 @@
+use getopts::{optflag,usage,OptGroup};
+use std::io::{println,print};
+
+pub fn opts() -> [OptGroup, .. 2]{
+    return [
+        optflag("v", "version", "display the version"),
+        optflag("h", "help", "print this help menu")
+    ]
+}
+
+pub fn print_usage(_program: &str, opts: &[OptGroup]) {
+    let space = "    ";
+    println("Usage: muxed [options]");
+    println("");
+    println("Commands:");
+    println(format!("{}new                 create a new project file", space).as_slice());
+    print(format!("{}open                open a new project file", space).as_slice());
+    println(usage("", opts).as_slice());
+}
