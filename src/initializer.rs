6821c2d2358be72f5eebaa8ecc21a43da8a06133
Brian Pearce <brian.o.pearce@gmail.com>
2014-07-24 00:02:25 -0700

    Pass a reference instead of the object.

--- src/initializer.rs
+++ src/initializer.rs
@@ -6,7 +6,7 @@ use editor;
 pub fn init(args: Vec<String>) {
     let program = args[0].clone();
     let opts = help::opts();
-    let matches = get_matches(args.tail(), opts.clone());
+    let matches = &get_matches(args.tail(), opts.clone());
 
     if matches.opt_present("h") || !validate_command(matches) {
         help::print_usage(program.as_slice(), opts);
@@ -23,9 +23,11 @@ fn get_matches(tail: &[String], opts: &[OptGroup]) -> Matches {
     }
 }
 
-fn validate_command(matches: Matches) -> bool {
+fn validate_command(matches: &Matches) -> bool {
     let command = if matches.free.len() == 2 {
-        matches.free[0].clone()
+        matches.free[0].as_slice()
+    } else {
+      return false
     };
 
     return match command {
@@ -35,9 +37,9 @@ fn validate_command(matches: Matches) -> bool {
     }
 }
 
-fn run_command(matches: Matches) {
-    let command = matches.free[0].clone();
-    let value = matches.free[1].clone();
+fn run_command(matches: &Matches) {
+    let command = matches.free[0].as_slice();
+    let value = matches.free[1].as_slice();
 
     match command {
         "new"  => creator::new(value),
@@ -48,19 +50,19 @@ fn run_command(matches: Matches) {
 
 #[test]
 fn validate_command_new_returns_true() {
-    let matches = get_matches([String::from_str("new muxed")], help::opts());
+    let matches = &get_matches([String::from_str("new"), String::from_str("muxed")], help::opts());
     assert_eq!(validate_command(matches), true);
 }
 
 #[test]
 fn validate_command_open_returns_true() {
-    let matches = get_matches([String::from_str("open muxed")], help::opts());
+    let matches = &get_matches([String::from_str("open"), String::from_str("muxed")], help::opts());
     assert_eq!(validate_command(matches), true);
 }
 
 #[test]
 fn validate_command_value_returns_false() {
-    let matches = get_matches([String::from_str("value")], help::opts());
+    let matches = &get_matches([String::from_str("value"), String::from_str("muxed")], help::opts());
     assert_eq!(validate_command(matches), false);
 }
 
