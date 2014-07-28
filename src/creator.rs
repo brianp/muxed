e22b1f072efb315e786ca5d876a556a9c6e495ae
Brian Pearce <brian.o.pearce@gmail.com>
2014-07-27 23:36:58 -0700

    Don't use relative user paths. Use appropriate postfix path for homedir.

--- src/creator.rs
+++ src/creator.rs
@@ -1,7 +1,9 @@
 use std::io::File;
+use std::os::{homedir};
 
 pub fn new(name: &str) {
-  let path = format!("~/.muxed/{}", name);
+  let home = homedir().unwrap().display();
+  let path = format!("{}/.muxed/{}", home, name);
   println!("{}", path);
   let mut file = File::create(&Path::new(path));
   file.write(b"This is a sample file");
