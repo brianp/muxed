84656e28b5b514e894729460962edcda92289e9a
Brian Pearce <brian.o.pearce@gmail.com>
2014-07-29 01:13:21 -0700

    Create the mixed dir if it does not exist.

--- src/creator.rs
+++ src/creator.rs
@@ -9,6 +9,23 @@ pub fn new<'r>(name: &str) {
     File::create(&Path::new(path));
 }
 
+fn create_muxed_dir(name: &String) -> Path {
+    let home_unwrap = homedir().unwrap();
+    let path = &Path::new(format!("{}/.{}", home_unwrap.display(), name));
+    fs::mkdir(path, ::std::io::UserRWX);
+    path.clone()
+}
+#[test]
+fn creates_muxed_dir() {
+    let name        = format!("test_project_{}", random::<f64>());
+    let dir         = format!("test_dir_{}", random::<f64>());
+    let home_unwrap = homedir().unwrap();
+    let muxed_path  = &Path::new(format!("{}/.{}/", home_unwrap.display(), dir.as_slice()));
+    create_muxed_dir(&dir);
+    assert!(muxed_path.exists());
+    fs::rmdir_recursive(muxed_path);
+}
+
 #[test]
 fn new_writes_file_to_muxed_dir() {
     let name = format!("test_project_{}", random::<f64>());
