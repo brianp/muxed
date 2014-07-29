7eecf7f26cbdba4eb252aaee4328d808a878bd1b
Brian Pearce <brian.o.pearce@gmail.com>
2014-07-28 22:44:39 -0700

    Don't let the unwrap have an expression length lifetime.

--- src/creator.rs
+++ src/creator.rs
@@ -1,10 +1,8 @@
 use std::io::File;
 use std::os::{homedir};
 
-pub fn new(name: &str) {
-  let home = homedir().unwrap().display();
-  let path = format!("{}/.muxed/{}", home, name);
-  println!("{}", path);
-  let mut file = File::create(&Path::new(path));
-  file.write(b"This is a sample file");
+pub fn new<'r>(name: &str) {
+  let home_unwrap = homedir().unwrap();
+  let path = format!("{}/.muxed/{}", home_unwrap.display(), name);
+  File::create(&Path::new(path));
 }
