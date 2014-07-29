b066e943d20189caec3bc62d6eb719e94191d0be
Brian Pearce <brian.o.pearce@gmail.com>
2014-07-28 23:56:01 -0700

    Test to ensure file is created.

--- src/creator.rs
+++ src/creator.rs
@@ -1,8 +1,18 @@
 use std::io::File;
+use std::path::posix::Path;
 use std::os::{homedir};
 
 pub fn new<'r>(name: &str) {
     let home_unwrap = homedir().unwrap();
     let path = format!("{}/.muxed/{}", home_unwrap.display(), name);
     File::create(&Path::new(path));
+}
+
+#[test]
+fn new_writes_file_to_muxed_dir() {
+    let name = "test_project";
+    let home_unwrap = homedir().unwrap();
+    let path = &Path::new(format!("{}/.muxed/{}", home_unwrap.display(), name));
+    new(name);
+    assert!(path.exists());
 }
