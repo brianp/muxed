f545b87776b85a3c1b6b3b5b3743ce9dd7ecdcd1
Brian Pearce <brian.o.pearce@gmail.com>
2014-07-28 23:55:51 -0700

    Fix indent.

--- src/creator.rs
+++ src/creator.rs
@@ -2,7 +2,7 @@ use std::io::File;
 use std::os::{homedir};
 
 pub fn new<'r>(name: &str) {
-  let home_unwrap = homedir().unwrap();
-  let path = format!("{}/.muxed/{}", home_unwrap.display(), name);
-  File::create(&Path::new(path));
+    let home_unwrap = homedir().unwrap();
+    let path = format!("{}/.muxed/{}", home_unwrap.display(), name);
+    File::create(&Path::new(path));
 }
