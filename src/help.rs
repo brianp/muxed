da8a33a04c07ab75169182a3776898b51d144cfb
Brian Pearce <brian.o.pearce@gmail.com>
2014-08-03 12:34:49 -0700

    Vanity test for opts. Allow dead code for print usage.

--- src/help.rs
+++ src/help.rs
@@ -8,6 +8,7 @@ pub fn opts() -> [OptGroup, .. 2]{
     ]
 }
 
+#[allow(dead_code)]
 pub fn print_usage(_program: &str, opts: &[OptGroup]) {
     let space = "    ";
     println("Usage: muxed <command> [options]");
@@ -16,4 +17,9 @@ pub fn print_usage(_program: &str, opts: &[OptGroup]) {
     println(format!("{}new [name]          create a new project file", space).as_slice());
     print(format!("{}open [name]         open a new project file", space).as_slice());
     println(usage("", opts).as_slice());
+}
+
+#[test]
+fn opts_returns_two_options() {
+  assert_eq!(opts().len(), 2);
 }
