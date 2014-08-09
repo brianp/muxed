ad5c4ac6f3c79b9f2121f95dde94b269c52cce31
Brian Pearce <brian.o.pearce@gmail.com>
2014-08-09 13:50:13 -0700

    We don't need to apply cfg(test) to everything as this module is only loaded under those circumstances.

--- src/test_helper.rs
+++ src/test_helper.rs
@@ -1,7 +1,6 @@
-#[cfg(test)] use std::rand::random;
+use std::rand::random;
 
 /// Test helper to standardize how random files and directories are generated.
-#[cfg(test)]
 pub fn random_name() -> String {
     format!("test_{}", random::<f64>())
 }
