6e361e6eca051bc3f0e50d7439e41e59be29bad8
Brian Pearce <brian.o.pearce@gmail.com>
2014-08-09 14:05:26 -0700

    Add a method to quickly clean up the files created during testing.

--- src/test_helper.rs
+++ src/test_helper.rs
@@ -1,7 +1,14 @@
 use std::rand::random;
+use std::io::fs;
 
 /// Test helper to standardize how random files and directories are generated.
 pub fn random_name() -> String {
     format!("test_{}", random::<f64>())
 }
 
+pub fn cleanup_file(path: &Path) {
+    match fs::unlink(path) {
+        Ok(()) => (), // succeeded
+        Err(e) => println!("Failed to unlink the path {} with error {}", path.display(), e),
+    }
+}
