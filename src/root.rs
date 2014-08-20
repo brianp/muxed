de0fd4788f171ae188158e6c7e2186775ad58317
Brian Pearce <brian.o.pearce@gmail.com>
2014-08-19 23:36:52 -0700

    I can't lie. This was written a few days ago and I forget what lead to the changes.

--- src/root.rs
+++ src/root.rs
@@ -5,8 +5,10 @@ use std::os::homedir;
 
 static MUXED_NAME_STR: &'static str = "muxed";
 
+pub fn tester() { }
+pub fn tester(path: Path) { } 
 /// Create the muxed directory and return the path if creation is successful.
-pub fn create_muxed_dir(name: &Path) -> Path {
+pub fn create_muxed_dir(path: &Path) -> Path {
     match fs::mkdir(path, ::std::io::UserRWX) {
         Ok(()) => (),
         Err(_e) => println!("Failed to create muxed directory: {}", path.filename())
@@ -29,26 +31,25 @@ pub fn muxed_dir() -> Path {
     }
 }
 
-#[test]
-fn muxed_dir_exists_returns_false() {
-    assert!(!muxed_dir_exists(&random_name()));
+#[cfg(test)] pub fn muxed_path() -> Path {
+    Path::new(format!("/tmp/.muxed_{}/", random_name()))
 }
 
-#[test]
-fn muxed_dir_exists_returns_true() {
-    let dir = random_name();
-    create_muxed_dir(&dir);
-    assert!(muxed_dir_exists(&dir));
-
-    let muxed_path  = &Path::new(format!("{}/.{}/", homedir_string(), dir));
-    cleanup_dir(muxed_path);
+#[cfg(not_test)] pub fn muxed_path() -> Path {
+    Path::new(format!("{}/.{}/", root::homedir_string(), &MUXED_NAME_STR.to_string()))
 }
 
 #[test]
 fn creates_muxed_dir() {
-    let dir = random_name();
-    let muxed_path  = &Path::new(format!("{}/.{}/", homedir_string(), dir));
-    create_muxed_dir(&dir);
+    let muxed_path = &muxed_path();
+    create_muxed_dir(muxed_path);
+    assert!(muxed_path.exists());
+    cleanup_dir(muxed_path);
+}
+
+#[test]
+fn muxed_dir_creates_dir() {
+    let muxed_path = &muxed_dir();
     assert!(muxed_path.exists());
     cleanup_dir(muxed_path);
 }
