e277d19105d74573658a268e4972d53f995aef7b
Brian Pearce <brian.o.pearce@gmail.com>
2014-10-13 16:13:22 -0700

    Refactor to make use of Some, None, & Maybes
    When returning options use Some and None to help with error cases.
    Previously passing unknown opts would cause exceptions.

--- src/arg_parse.rs
+++ src/arg_parse.rs
@@ -1,31 +1,30 @@
 use getopts::{getopts,OptGroup,Matches};
 #[cfg(test)] use help;
 
-pub fn command(matches: &Matches) -> &str {
-    matches.free[0].as_slice()
+pub fn command(fragments: &Vec<String>) -> &String {
+    &fragments[0]
 }
 
-pub fn value(matches: &Matches) -> &str {
-    matches.free[1].as_slice()
+pub fn value(fragments: &Vec<String>) -> &String {
+    &fragments[1]
 }
 
 /// Accept the current arguments and match them using getopts.
 /// # Errors
 /// Will fail if arguments are not found.
-pub fn matches(tail: &[String], opts: &[OptGroup]) -> Matches {
+pub fn matches(tail: &[String], opts: &[OptGroup]) -> Option<Matches> {
     return match getopts(tail, opts) {
-        Ok(m) => { m }
-        Err(f) => { fail!(f.to_string()) }
+        Ok(m) => { Some(m) }
+        Err(_) => { None }
     }
 }
 
 /// Validate the free text passed to the application. If the free text can match
 /// an application command return `true` otherwise `false`.
 pub fn valid_command(matches: &Matches) -> bool {
-    let command = if matches.free.len() == 2 {
-        matches.free[0].as_slice()
-    } else {
-        return false
+    let command = match matches.free.len() {
+        2 => matches.free[0].as_slice(),
+        _ => return false
     };
 
     return match command {
@@ -37,42 +36,52 @@ pub fn valid_command(matches: &Matches) -> bool {
 
 #[test]
 fn command_returns_new() {
-    let matches = &matches([String::from_str("new"), String::from_str("muxed")], help::opts());
-    assert_eq!(command(matches), "new");
+    let matches = &matches([String::from_str("new"), String::from_str("muxed")], help::opts()).unwrap();
+    assert_eq!(command(&matches.free), &String::from_str("new"));
 }
 
 #[test]
 fn command_returns_edit() {
-    let matches = &matches([String::from_str("open"), String::from_str("muxed")], help::opts());
-    assert_eq!(command(matches), "open");
+    let matches = &matches([String::from_str("open"), String::from_str("muxed")], help::opts()).unwrap();
+    assert_eq!(command(&matches.free), &String::from_str("open"));
 }
 
 #[test]
 fn value_returns_muxed() {
-    let matches = &matches([String::from_str("new"), String::from_str("muxed")], help::opts());
-    assert_eq!(value(matches), "muxed");
+    let matches = &matches([String::from_str("new"), String::from_str("muxed")], help::opts()).unwrap();
+    assert_eq!(value(&matches.free), &String::from_str("muxed"));
 }
 
 #[test]
-#[should_fail]
+fn value_returns_middle() {
+    let matches = &matches([String::from_str("new"), String::from_str("middle"), String::from_str("end")], help::opts()).unwrap();
+    assert_eq!(value(&matches.free), &String::from_str("middle"));
+}
+
+#[test]
 fn matches_returns_failure_with_bad_opts() {
-    matches([String::from_str("-m")], help::opts());
+    assert!(matches([String::from_str("-m")], help::opts()).is_none());
+}
+
+#[test]
+fn matches_returns_with_good_opts() {
+    assert!(matches([String::from_str("-v")], help::opts()).is_some());
 }
 
 #[test]
 fn valid_command_new_returns_true() {
-    let matches = &matches([String::from_str("new"), String::from_str("muxed")], help::opts());
+    let matches = &matches([String::from_str("new"), String::from_str("muxed")], help::opts()).unwrap();
     assert_eq!(valid_command(matches), true);
 }
 
 #[test]
 fn valid_command_open_returns_true() {
-    let matches = &matches([String::from_str("open"), String::from_str("muxed")], help::opts());
+    let matches = &matches([String::from_str("open"), String::from_str("muxed")], help::opts()).unwrap();
     assert_eq!(valid_command(matches), true);
 }
 
 #[test]
 fn valid_command_value_returns_false() {
-    let matches = &matches([String::from_str("value"), String::from_str("muxed")], help::opts());
+    let matches = &matches([String::from_str("value"), String::from_str("muxed")], help::opts()).unwrap();
     assert_eq!(valid_command(matches), false);
 }
--- src/main.rs
+++ src/main.rs
@@ -20,18 +20,27 @@ fn main() {
     let args: Vec<String> = os::args();
 
     let opts = help::opts();
-    let matches = &arg_parse::matches(args.tail(), opts.clone());
+    let maybe_matches = arg_parse::matches(args.tail(), opts.clone());
 
-    if matches.opt_present("v") {
-        println(format!("{}", "Version: 0.0.0").as_slice());
-        return;
-    } else if matches.opt_present("h") || !arg_parse::valid_command(matches) {
+    if maybe_matches.is_none() {
         help::print_usage(opts);
         return;
     }
 
-    let command = arg_parse::command(matches);
-    let _value  = arg_parse::value(matches);
+    let matches = maybe_matches.unwrap();
+
+    if matches.opt_present("h") || !arg_parse::valid_command(&matches) {
+        help::print_usage(opts);
+        return;
+    } else if matches.opt_present("v") {
+        println(format!("{}", "Version: 0.0.0").as_slice());
+        return;
+    }
+
+    let fragments = &matches.free;
+
+    let command = arg_parse::command(fragments);
+    let _value  = arg_parse::value(fragments);
 
     match command {
 //        "new"  => creator::new(value),
