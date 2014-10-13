0e53d99282909cac70ef9d4ad539e679741f29ce
Brian Pearce <brian.o.pearce@gmail.com>
2014-10-13 02:59:03 -0700

    Add periods.

--- src/help.rs
+++ src/help.rs
@@ -3,19 +3,19 @@ use std::io::{println,print};
 
 pub fn opts() -> [OptGroup, .. 2]{
     return [
-        optflag("v", "version", "display the version"),
-        optflag("h", "help", "print this help menu")
+        optflag("v", "version", "display the version."),
+        optflag("h", "help", "print this help menu.")
     ]
 }
 
 #[allow(dead_code)]
-pub fn print_usage(_program: &str, opts: &[OptGroup]) {
+pub fn print_usage(opts: &[OptGroup]) {
     let space = "    ";
     println("Usage: muxed <command> [options]");
     println("");
     println("Commands:");
-    println(format!("{}new [name]          create a new project file", space).as_slice());
-    print(  format!("{}open [name]         open an existing project file", space).as_slice());
+    println(format!("{}new [name]          create a new project file.", space).as_slice());
+    print(  format!("{}open [name]         open an existing project file.", space).as_slice());
     println(usage("", opts).as_slice());
 }
 
