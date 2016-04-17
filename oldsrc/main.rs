0749cf6e3c79e86555fbb5dbfae60664ebd9560e
Brian Pearce <brian.o.pearce@gmail.com>
2016-04-17 12:34:12 -0700

    Prepping for a reset
    This project was originally built pre Rust v1.0. It no longer compiles and many breaking changes have occured.
    Move old src for reference and begin laying basic groundwork for new code.

---
+++ oldsrc/main.rs
@@ -0,0 +1,55 @@
+//! Muxed. A tmux project manager with no runtime dependencies.
+extern crate getopts;
+extern crate libc;
+
+use std::os;
+use std::io::{println};
+
+#[macro_export]
+macro_rules! try_or_err (
+    ($expr: expr, $err: expr) => ({
+        match $expr {
+            Ok(val) => val,
+            Err(_e) => println!($err)
+        }
+    })
+);
+
+mod help;
+mod arg_parse;
+mod root;
+mod project;
+mod test_helper;
+
+/// The main execution method.
+/// Verify all the arguments and options passed are valid for the application.
+fn main() {
+    let args: Vec<String> = os::args();
+
+    let opts = &help::opts();
+    let maybe_matches = arg_parse::matches(args.tail(), opts);
+
+    if maybe_matches.is_none() {
+        help::print_usage(opts);
+        return;
+    }
+
+    let matches = maybe_matches.unwrap();
+
+    if matches.opt_present("v") {
+        println(format!("{}", "Version: 0.0.1").as_slice());
+        return;
+    } else if matches.opt_present("h") || !arg_parse::valid_command(&matches) {
+        help::print_usage(opts);
+        return;
+    }
+
+    let fragments = &matches.free;
+    let command   = arg_parse::command(fragments);
+    let file_path = arg_parse::file_path(&root::path(), fragments);
+
+    match command {
+        "new"  => project::main(file_path),
+        _      => help::print_usage(opts)
+    }
+}
