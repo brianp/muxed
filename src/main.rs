3e9c7ff4c145e47083926fe7e05045a43f4d2298
Brian Pearce <brian.o.pearce@gmail.com>
2016-04-18 00:22:56 -0700

    Run the tmux ffi
    Utilize the new tmux ffi to open a new named session.

--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,16 @@
 extern crate getopts;
 extern crate libc;
 
+use std::env;
+
+mod tmux;
 
 /// The main execution method.
 /// Verify all the arguments and options passed are valid for the application.
 fn main() {
+    let args: Vec<String> = env::args().collect();
+    let program = args[0].clone();
+    let input = args[1].clone();
 
-
-    //match command {
-    //    "new"  => project::main(file_path),
-    //}
+    tmux::open(input);
 }
---
+++ src/project/mod.rs
@@ -0,0 +1,3 @@
+/// The main execution method.
+fn open(project_name: Vec<String>) {
+}
