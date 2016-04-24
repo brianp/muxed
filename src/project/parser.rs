f09895da583a0cb5f95deac4857c5c9565b9e6df
Brian Pearce <brian.o.pearce@gmail.com>
2016-04-23 18:35:58 -0700

    WIP: Parse okay but don't really do anything.

--- src/project/parser.rs
+++ src/project/parser.rs
@@ -1,1 +1,41 @@
+use std::collections::BTreeMap;
+use yaml_rust::Yaml;
 use tmux;
+
+pub struct Command {
+    //commands: Vec<Command>,
+    key: String,
+    value: String
+}
+
+pub fn main(yaml_string: &Vec<Yaml>) -> Vec<Command> {
+    let mut commands: Vec<Command> = vec!();
+    //println!("{:?}", yaml_string);
+
+    //fn yaml_match(y: &Yaml) {
+    //    match y {
+    //        &Yaml::Array(ref v) => {
+    //            for x in v {
+    //                yaml_match(x);
+    //            }
+    //        },
+    //        &Yaml::Hash(ref h) => {
+    //            for (k, v) in h {
+    //                println!("hash: {:?}:", k);
+    //                yaml_match(v);
+    //            }
+    //        },
+    //        _ => print!("{:?}", y)
+    //    }
+    //};
+
+    //for line in yaml_string {
+    //    for window in yaml_match(line["windows"]) {
+    //        let value = yaml_match(window);
+    //        let command = Command{key: "window", value: value};
+    //        commands.push(command);
+    //    };
+    //};
+
+    commands
+}
