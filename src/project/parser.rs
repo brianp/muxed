888bb582e6f2ede0ad32eb5fabe8c485a4158188
Brian Pearce <brian.o.pearce@gmail.com>
2016-04-23 21:18:05 -0700

    Parse and create build commands for new windows.

--- src/project/parser.rs
+++ src/project/parser.rs
@@ -2,6 +2,7 @@ use std::collections::BTreeMap;
 use yaml_rust::Yaml;
 use tmux;
 
+#[derive(Debug)]
 pub struct Command {
     //commands: Vec<Command>,
     key: String,
@@ -12,30 +13,41 @@ pub fn main(yaml_string: &Vec<Yaml>) -> Vec<Command> {
     let mut commands: Vec<Command> = vec!();
     //println!("{:?}", yaml_string);
 
-    //fn yaml_match(y: &Yaml) {
-    //    match y {
-    //        &Yaml::Array(ref v) => {
-    //            for x in v {
-    //                yaml_match(x);
-    //            }
-    //        },
-    //        &Yaml::Hash(ref h) => {
-    //            for (k, v) in h {
-    //                println!("hash: {:?}:", k);
-    //                yaml_match(v);
-    //            }
-    //        },
-    //        _ => print!("{:?}", y)
-    //    }
-    //};
+    fn yaml_match(y: &Yaml) {
+        match y {
+            &Yaml::Array(ref v) => {
+                for x in v {
+                    yaml_match(x);
+                }
+            },
+            &Yaml::Hash(ref h) => {
+                for (k, v) in h {
+                    println!("hash: {:?}:", k);
+                    yaml_match(v);
+                }
+            },
+            _ => print!("{:?}", y)
+        };
+    };
 
-    //for line in yaml_string {
-    //    for window in yaml_match(line["windows"]) {
-    //        let value = yaml_match(window);
-    //        let command = Command{key: "window", value: value};
-    //        commands.push(command);
-    //    };
-    //};
+    for doc in yaml_string {
+        for window in doc["windows"].as_vec().unwrap() {
+            match window {
+                &Yaml::Array(ref a) => {
+                    for w in a {
+                      commands.push(Command{key: "window".to_string(), value: w.as_str().unwrap().to_string()})
+                    }
+                },
+                &Yaml::Hash(ref h)  => {
+                    for (k, v) in h {
+                        commands.push(Command{key: "window".to_string(), value: k.as_str().unwrap().to_string()})
+                    }
+                },
+                _ => panic!("wtf")
+            };
+        };
+    };
 
+    println!("{:?}", commands);
     commands
 }
