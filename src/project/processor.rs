8b3756e6d1e0f86a183865567a4d454ec37489c1
Brian Pearce <brian.o.pearce@gmail.com>
2016-04-24 15:49:54 -0700

    Add the processor.
    The processor's purpose is to read and execute on the commands being passed. The commands represent the system calls required to setup someones tmux env.

---
+++ src/project/processor.rs
@@ -0,0 +1,17 @@
+use std::collections::BTreeMap;
+use yaml_rust::Yaml;
+use project::parser::Command;
+use tmux;
+
+pub fn main(commands: &Vec<Command>) -> () {
+    let sess = "test".to_string();
+
+    let (first_window, exec_commands) = commands.split_at(1);
+    tmux::new_session(sess.clone(), first_window[0].value.clone());
+
+    for w in exec_commands {
+        tmux::new_window(sess.clone(), w.value.clone());
+    };
+
+    tmux::open(sess.clone());
+}
