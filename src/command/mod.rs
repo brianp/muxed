5bc5dd839ccddf2fa5c7a73be92ca60018653de6
Brian Pearce <brian.o.pearce@gmail.com>
2016-04-30 01:46:02 -0700

    Add new available command enums
    After running version 0.1.0 with tmux call output, it's obvious the current commands are being overused.
    Add new commands to better reflect the actions being taken.

--- src/command/mod.rs
+++ src/command/mod.rs
@@ -1,23 +1,63 @@
 #[derive(Debug)]
 #[derive(Clone)]
+pub struct Session {
+    pub name: String,
+    pub window_name: String,
+    pub root: Option<String>
+}
+
+#[derive(Debug)]
+#[derive(Clone)]
 pub struct Window {
-  pub value: String,
-  pub root: Option<String>,
-  pub exec: String
+    pub session_name: String,
+    pub name: String,
+    pub root: Option<String>
+}
+
+#[derive(Debug)]
+#[derive(Clone)]
+pub struct Split {
+    pub target: String,
+    pub root: Option<String>
+}
+
+#[derive(Debug)]
+#[derive(Clone)]
+pub struct Layout {
+    pub target: String,
+    pub layout: String
+}
+
+#[derive(Debug)]
+#[derive(Clone)]
+pub struct SendKeys {
+    pub target: String,
+    pub exec: String
+}
+
+#[derive(Debug)]
+#[derive(Clone)]
+pub struct Attach {
+    pub name: String
 }
 
 #[derive(Debug)]
 #[derive(Clone)]
 pub struct Panes {
-  pub layout: String,
-  pub window: String,
-  pub exec: Vec<String>,
-  pub root: Option<String>
+    pub layout: String,
+    pub window: String,
+    pub exec: Vec<String>,
+    pub root: Option<String>
 }
 
 #[derive(Debug)]
 #[derive(Clone)]
 pub enum Command {
+    Session(Session),
     Window(Window),
+    Split(Split),
+    Layout(Layout),
+    SendKeys(SendKeys),
+    Attach(Attach),
     Panes(Panes)
 }
