84c81a62ce22be1b4e84571e0561a87696dc764e
Brian Pearce <brian.o.pearce@gmail.com>
2016-05-09 00:29:41 -0700

    Begin writing first round of code docs.

--- src/main.rs
+++ src/main.rs
@@ -14,8 +14,28 @@ use project::parser;
 use project::processor;
 
 /// The main execution method.
-/// Verify all the arguments and options passed are valid for the application.
-fn main() {
+/// Currently accepts a single option. The option represents a configuration
+/// file in the same naming format. Given a project file name `projectName.yml`
+/// in the `~/.muxed/` directory.
+///
+/// # Examples
+///
+/// ~/.muxed/projectName.yml
+///
+/// ```
+/// root: ~/projects/muxed/
+/// windows:
+///     - cargo: "cargo build"
+///     - vim: "vim ."
+///     - git: ""
+/// ```
+///
+/// You can run the command:
+///
+/// ```
+/// $ ./muxed projectName
+/// ```
+pub fn main() {
     let args: Vec<String> = env::args().collect();
     //let program = args[0].clone();
     let input = args[1].clone();
