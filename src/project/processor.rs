dec9088d48adceae32f2850f8dd73f66a5500592
Brian Pearce <brian.o.pearce@gmail.com>
2016-06-01 12:23:33 -0700

    Spacing

--- src/project/processor.rs
+++ src/project/processor.rs
@@ -24,12 +24,12 @@ use tmux;
 pub fn main(commands: &Vec<Command>) -> () {
     for c in commands {
         match c {
-            &Command::Session(ref c)    => tmux::new_session(&c.name, &c.window_name),
-            &Command::Window(ref c)     => tmux::new_window(&c.session_name, &c.name),
-            &Command::Split(ref c)      => tmux::split_window(&c.target, &c.root),
-            &Command::Layout(ref c)     => tmux::layout(&c.target, &c.layout),
-            &Command::SendKeys(ref c)   => tmux::send_keys(&c.target, &c.exec),
-            &Command::Attach(ref c)     => tmux::attach(&c.name)
+            &Command::Session(ref c)  => tmux::new_session(&c.name, &c.window_name),
+            &Command::Window(ref c)   => tmux::new_window(&c.session_name, &c.name),
+            &Command::Split(ref c)    => tmux::split_window(&c.target, &c.root),
+            &Command::Layout(ref c)   => tmux::layout(&c.target, &c.layout),
+            &Command::SendKeys(ref c) => tmux::send_keys(&c.target, &c.exec),
+            &Command::Attach(ref c)   => tmux::attach(&c.name)
         }
     };
 }
