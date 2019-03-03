75c1b870d47ed78e1467634271eb448751126f54
Brian Pearce <brian.o.pearce@gmail.com>
2019-03-02 23:31:49 -0800

    refactor(Restructure): Move muxed read to module
    Move all existing muxed functionality into a new module to set the base
    for the merging of muxednew and muxedsnapshot.

---
+++ muxed/src/load/mod.rs
@@ -0,0 +1,33 @@
+use args::Args;
+use command::Command;
+use project::{parser, processor};
+use project;
+use tmux::config::Config;
+use tmux;
+
+pub fn exec(args: Args) -> Result<(), String> {
+    let muxed_dir = match args.flag_p {
+        Some(ref x) => Some(x.as_str()),
+        None => None,
+    };
+
+    let yaml = project::read(&args.arg_project, &muxed_dir).unwrap();
+    let project_name = &yaml[0]["name"]
+        .as_str()
+        .unwrap_or(&args.arg_project)
+        .to_string();
+
+    let commands: Vec<Command>;
+    match project::session_exists(project_name) {
+        Some(c) => {
+            commands = vec![c];
+        }
+        None => {
+            let config = Config::from_string(tmux::get_config());
+            commands = parser::call(&yaml, project_name, args.flag_d, &config).expect("Couldn't parse commands");
+        }
+    };
+
+    processor::main(&commands);
+    Ok(())
+}
