0852d6a45281e8603ac14eae06c973d1c070cb51
Brian Pearce <brian.o.pearce@gmail.com>
2019-12-11 17:09:50 +0100

    Add rand_names module
    This is to help standardize the way project paths, and files are generated during testing.

--- common/src/lib.rs
+++ common/src/lib.rs
@@ -4,3 +4,4 @@ extern crate rand;
 pub mod args;
 pub mod first_run;
 pub mod project_paths;
+pub mod rand_names;
---
+++ common/src/rand_names.rs
@@ -0,0 +1,30 @@
+use rand::random;
+use std::path::PathBuf;
+use project_paths::CONFIG_EXTENSION;
+
+pub fn project_path_name() -> String {
+    format!("/tmp/.muxed-test-{}/", random::<u16>())
+}
+
+pub fn project_path() -> PathBuf {
+    PathBuf::from(project_path_name())
+}
+
+pub fn project_file_name() -> String {
+    format!("muxed-test-project-{}", random::<u16>())
+}
+
+pub fn project_file_path() -> PathBuf {
+    let project_file = PathBuf::from(project_file_name()).with_extension(CONFIG_EXTENSION);
+    project_path().join(project_file)
+}
+
+pub fn project_file_path_with_name(name: &str) -> PathBuf {
+    let project_file = PathBuf::from(name).with_extension(CONFIG_EXTENSION);
+    project_path().join(project_file)
+}
+
+pub fn project_file_with_dir(dir: &str) -> PathBuf {
+    let project_file = PathBuf::from(project_file_name()).with_extension(CONFIG_EXTENSION);
+    PathBuf::from(dir).join(project_file)
+}
